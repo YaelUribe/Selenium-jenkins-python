a1cbabd156ecf7fc0af583a3063e3ea8dbc64f1f
Yael_dev <yael_dev@Uribe.Luque>
2023-01-09 16:13:02 -0500

    updating fixtures so running the tests in headless mode is available

--- tests_web/staging/conftest.py
+++ tests_web/staging/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 import os
 
@@ -7,8 +8,10 @@ import os
 @pytest.fixture(scope='class')
 def init_driver(request):
     time.sleep(5)
-    web_driver = webdriver.Chrome(executable_path=os.getcwd()+"/chromedriver")
-    web_driver.maximize_window()
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    web_driver = webdriver.Chrome(executable_path=os.getcwd()+"/chromedriver", options=options)
     request.cls.driver = web_driver
     yield
     web_driver.close()
